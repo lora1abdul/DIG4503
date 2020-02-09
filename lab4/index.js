00c1174b464b58dd6877e971597bd58a08f851f1
Lora Abdulhak <loraabdulhak@hotmail.com>
2020-02-08 22:31:45 -0500

    Update index.js

--- lab4/index.js
+++ lab4/index.js
@@ -4,7 +4,6 @@ const port = 80;
 const pokemon = require('json-pokemon');
 const chalk = require("chalk");
 
-//find id in pokemon array
 
 App.get("/id/:id", function(req, res){
     pokemon.forEach((value)=>{
@@ -20,7 +19,6 @@ console.log(chalk.red(req.path));
 
 
 
-//find name in pokemon array
 App.get("/name/:name", function(req, res){
     pokemon.forEach((value)=>{
         if(value.name ==req.params.name){
@@ -38,14 +36,14 @@ console.log(chalk.red(req.path));
 
 
 
-//console log when request is made on the server
+
 App.get("/", function(req, res){
     console.log("Got a request");
     console.log(chalk.green(req.path));
     res.send("");
  });
 
- //listen on port 80
+ 
 App.listen(port, function(){
     console.log("server running");
    }
